8581234c5c0d52c57df7b24ec11ee7cd1a5af0c0
Riela <sohee.ghime@gmail.com>
2018-08-14 14:05:39 +0900

    first commit
    fc

---
+++ _ViewModelR/app/src/main/java/com/android/riela/livedatatestr/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.android.riela.livedatatestr
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("00----- ","onCreate")
+    }
+}
