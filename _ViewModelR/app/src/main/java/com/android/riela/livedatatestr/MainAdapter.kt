da665873a65c37d4be49b85679bfd4d27dd677b4
Riela <riela.78@kakaocorp.com>
2018-08-26 21:52:57 +0900

    list item layout

--- _ViewModelR/app/src/main/java/com/android/riela/livedatatestr/MainAdapter.kt
+++ _ViewModelR/app/src/main/java/com/android/riela/livedatatestr/MainAdapter.kt
@@ -1,5 +1,6 @@
 package com.android.riela.livedatatestr
 
+import android.bluetooth.BluetoothDevice
 import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
@@ -21,10 +22,12 @@ class MainAdapter (private val context: Context,
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         Log.e("R","--- getView ")
-        val rowView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+        val rowView = inflater.inflate(R.layout.list_item, parent, false)
         val row = getItem(position) as String
-        val tv = rowView.findViewById(android.R.id.text1) as TextView
+        val tv = rowView.findViewById(R.id.text11) as TextView
+        val subTv = rowView.findViewById(R.id.text22) as TextView
         tv.text = row
+        subTv.text = "sub text"
         return rowView
     }
 
